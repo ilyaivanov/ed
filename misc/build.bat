d7d3f9f1cd9cdc60cfd9d934d95af932af3afec2
Ilya Ivanov <static.ila@gmail.com>
2025-05-08 12:06:43 +0300

    Remove optimization to enable debug

--- misc/build.bat
+++ misc/build.bat
@@ -1,7 +1,8 @@
 @echo off
 REM del main.exe
-clang main.c -g -O3 -ffast-math -o build\main.exe -lkernel32 -luser32 -lgdi32.lib -ldwmapi.lib 
+clang main.c -g -o build\main.exe -lkernel32 -luser32 -lgdi32.lib -ldwmapi.lib 
 
+REM -O3 -ffast-math 
 REM -Wall -Wextra
 if %ERRORLEVEL% EQU 0 (
     pushd build
