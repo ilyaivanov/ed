00102296564bf5945dfeb0ada82fe663930cf8f5
Ilya Ivanov <static.ila@gmail.com>
2025-07-17 14:06:56 +0300

    Update sample a bit

--- v3/console.cpp
+++ v3/console.cpp
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+void repeatchar(int count, char ch) {
+  for (int i = 0; i < count; i++)
+    printf("%c", ch);
+}
+
 int main() {
-  for (int i = 0; i < 12; i++)
-    printf("foo the the bar\n");
+  int size = 20;
+  for (int i = 1; i <= size; i += 2) {
+    repeatchar((size - i) / 2, ' ');
+    repeatchar(i, '*');
+    printf("\n");
+  }
   return 0;
 }
